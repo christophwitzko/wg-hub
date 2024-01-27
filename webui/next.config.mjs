ff20963c60b3af92e9228d902921dbcc45ddebf7
Christoph Witzko <github@christophwitzko.com>
2024-01-27 19:35:29 +0100

    fix: content type header

--- webui/next.config.mjs
+++ webui/next.config.mjs
@@ -1,4 +1,20 @@
+const isDev = process.env.NODE_ENV !== "production";
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: "export",
+  ...(isDev
+    ? {
+        rewrites() {
+          return [
+            {
+              source: "/api/:path*",
+              destination: "http://192.168.0.254/api/:path*",
+            },
+          ];
+        },
+      }
+    : {}),
+};
 
 export default nextConfig;
