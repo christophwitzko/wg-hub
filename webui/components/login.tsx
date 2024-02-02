931ebff4c7283155f58c8da15f54537131eb0ed7
Christoph Witzko <github@christophwitzko.com>
2024-02-02 15:41:07 +0100

    style: format

--- webui/components/login.tsx
+++ webui/components/login.tsx
@@ -1,5 +1,9 @@
 "use client";
 
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+
 import {
   Card,
   CardHeader,
@@ -7,62 +11,90 @@ import {
   CardContent,
 } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
 import { Button } from "@/components/ui/button";
 import { Alert, AlertTitle, AlertDescription } from "@/components/ui/alert";
 import { AlertCircle } from "lucide-react";
-import { useState } from "react";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
 import { useAuth } from "@/lib/auth";
 import { Center } from "@/components/center";
 
-export function Login() {
-  const [username, setUsername] = useState("admin");
-  const [password, setPassword] = useState("");
-  const auth = useAuth();
+const loginFormSchema = z.object({
+  username: z.string().min(1).max(50),
+  password: z.string().min(1).max(50),
+});
 
-  const buttonDisabled = auth.isLoading || !username || !password;
+export function Login() {
+  const form = useForm<z.infer<typeof loginFormSchema>>({
+    resolver: zodResolver(loginFormSchema),
+    defaultValues: {
+      username: "admin",
+    },
+  });
+  const auth = useAuth();
+  function onSubmit(values: z.infer<typeof loginFormSchema>) {
+    auth.login(values.username, values.password);
+  }
+
   return (
-    <Center asChild={true}>
-      <form>
-        <Card className="w-1/4 min-w-80 m-4">
-          <CardHeader>Login</CardHeader>
-          <CardContent className="grid w-full gap-4">
-            <div className="flex flex-col space-y-1.5">
-              <Label htmlFor="username">Username</Label>
-              <Input
-                id="username"
-                value={username}
-                onChange={(e) => setUsername(e.target.value)}
-              />
-            </div>
-            <div className="flex flex-col space-y-1.5">
-              <Label htmlFor="password">Password</Label>
-              <Input
-                id="password"
-                type="password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-              />
-            </div>
-            {auth.error && (
-              <Alert variant="destructive">
-                <AlertCircle className="size-4" />
-                <AlertTitle>Error</AlertTitle>
-                <AlertDescription>{auth.error}</AlertDescription>
-              </Alert>
-            )}
-          </CardContent>
-          <CardFooter className="flex justify-end">
-            <Button
-              type="submit"
-              disabled={buttonDisabled}
-              onClick={() => auth.login(username, password)}
-            >
-              Log in
-            </Button>
-          </CardFooter>
-        </Card>
-      </form>
-    </Center>
+    <Form {...form}>
+      <Center asChild={true}>
+        <form onSubmit={form.handleSubmit(onSubmit)}>
+          <Card className="w-1/4 min-w-80 m-4">
+            <CardHeader>Login</CardHeader>
+            <CardContent className="grid w-full gap-4">
+              <div className="flex flex-col space-y-1.5">
+                <FormField
+                  control={form.control}
+                  name="username"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Username</FormLabel>
+                      <FormControl>
+                        <Input {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              </div>
+              <div className="flex flex-col space-y-1.5">
+                <FormField
+                  control={form.control}
+                  name="password"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Password</FormLabel>
+                      <FormControl>
+                        <Input type="password" {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              </div>
+              {auth.error && (
+                <Alert variant="destructive">
+                  <AlertCircle className="size-4" />
+                  <AlertTitle>Error</AlertTitle>
+                  <AlertDescription>{auth.error}</AlertDescription>
+                </Alert>
+              )}
+            </CardContent>
+            <CardFooter className="flex justify-end">
+              <Button type="submit" disabled={auth.isLoading}>
+                Log in
+              </Button>
+            </CardFooter>
+          </Card>
+        </form>
+      </Center>
+    </Form>
   );
 }
